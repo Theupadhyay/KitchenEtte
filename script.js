e224af8c47a8845270aa17cb9d26e75699a97c43
Bhavya Garg <bebhavyagarg@gmail.com>
2023-10-28 12:52:37 +0530

    Changed file names and started adding functionalities

---
+++ script.js
@@ -0,0 +1,42 @@
+// Click functionalities
+$("nav #nav_bar_options #home").click(function () {
+  $(document).scrollTop(0);
+});
+
+$("nav img").click(function () {
+  $(document).scrollTop(0);
+});
+
+$("nav #nav_bar_options #menu").click(function () {
+  $(document).scrollTop(800);
+});
+
+$("nav #nav_bar_options #about-us").click(function () {
+  $(document).scrollTop(1550);
+});
+
+$("nav #nav_bar_options #cart").click(function () {
+  $(document).scrollTop(2400);
+});
+
+$("section#home #container button").click(function () {
+  $(document).scrollTop(800);
+});
+
+
+// hover functionalities
+// $("nav #nav_bar_options .opts").mouseenter(function () {
+//   $(this).stop().animate({ fontSize: "20px" });
+// });
+
+// $("nav #nav_bar_options .opts").mouseleave(function () {
+//     $(this).stop().animate({ fontSize: "17px" });
+//   });
+
+//   $("nav #nav_bar_options .opts svg").mouseenter(function () {
+//     $(this).stop().animate({ width: "50" },{height:"50"});
+//   });
+
+//   $("nav #nav_bar_options .opts svg").mouseleave(function () {
+//     $(this).stop().animate({ width: "50" },{height:"50"});
+//   });
